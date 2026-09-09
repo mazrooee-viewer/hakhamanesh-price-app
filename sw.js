93702065b7d84ccf42274b24e731464ee9315183
price-pwa publisher <publisher@hakhamanesh.local>
2026-09-09 13:36:19 +0330

    instant card open 1405/06/18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Service Worker — PWA لیست قیمت — build 20260909094408 */
-var CACHE = "price-pwa-20260909094408";
+/* Service Worker — PWA لیست قیمت — build 20260909100616 */
+var CACHE = "price-pwa-20260909100616";
 var SHELL = ["./", "./index.html", "./sales.html", "./manifest.json", "./manifest-sales.json",
   "./assets/app-icon-180.png", "./assets/app-icon-192.png", "./assets/app-icon-512.png"];
 
