c750f960d6c13e61af77195642c57f3c9b549e21
price-pwa publisher <publisher@hakhamanesh.local>
2026-09-10 13:20:27 +0330

    publish 2026-09-10 13:20

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Service Worker — PWA لیست قیمت — build 20260910094722 */
-var CACHE = "price-pwa-20260910094722";
+/* Service Worker — PWA لیست قیمت — build 20260910095024 */
+var CACHE = "price-pwa-20260910095024";
 var SHELL = ["./", "./index.html", "./sales.html", "./manifest.json", "./manifest-sales.json",
   "./assets/app-icon-180.png", "./assets/app-icon-192.png", "./assets/app-icon-512.png"];
 
