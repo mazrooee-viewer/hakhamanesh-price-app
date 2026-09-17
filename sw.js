36c1609e07fd2eed0966f563ee9dd2b3d2c9a09b
price-pwa publisher <publisher@hakhamanesh.local>
2026-09-17 16:25:46 +0330

    Hide sales extraNote from card face in Price Manager app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Service Worker — PWA لیست قیمت — build 20260916214417 */
-var CACHE = "price-pwa-20260916214417";
+/* Service Worker — PWA لیست قیمت — build 20260917125539 */
+var CACHE = "price-pwa-20260917125539";
 var SHELL = ["./", "./index.html", "./sales.html", "./manifest.json", "./manifest-sales.json",
   "./assets/app-icon-180.png", "./assets/app-icon-192.png", "./assets/app-icon-512.png"];
 
